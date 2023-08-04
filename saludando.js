7ebe43aa25f15df912167c2dc8c8e3d37be9f3da
vsandoval16 <vsandoval16@unab.edu.co>
2023-08-03 21:12:38 -0500

    Mision 6

---
+++ saludando.js
@@ -0,0 +1,10 @@
+var time=20;
+if (time >= 12 && time<=18) {
+    console.log("Buenas tardes")
+} else {
+    if (time < 12) {
+     console.log("Buenos diash")   
+    } else {
+     console.log("Buenas noches")   
+    }
+}
